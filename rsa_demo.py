b86176996163937fc056f1d6c4f0c67639c87a25
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2023-10-22 16:01:30 +0800

    Added a testFunc to show how timeit can work with local variables

--- rsa_demo.py
+++ rsa_demo.py
@@ -2,7 +2,15 @@
 # It compares the performance of those three algorithms using Timeit (more information on : https://docs.python.org/3/library/timeit.html#module-timeit)
 
 import timeit
+import random
 
-# Example
-execution_time = timeit.timeit('"-".join(str(n) for n in range(100))', number=10000)
-print("This took " + str(execution_time) + " seconds.");
+# This is used to better visualize the differences in runtime performance between the three algorithms.
+
+def testFunc():
+    array = [random.randint(1, 10000) for _ in range(10000)]
+    for number in array:
+        print(number)
+
+runtime_performance = timeit.timeit(stmt=testFunc, number=2)
+print(f"Execution time: {runtime_performance} seconds.")
+
