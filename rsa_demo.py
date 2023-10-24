6b6e6f9c3e3f1efd3dead8530b65d55e7467adde
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2023-10-24 21:50:57 +0800

    Refactored the partition function to make it easier to understand

--- rsa_demo.py
+++ rsa_demo.py
@@ -3,39 +3,27 @@
 import timeit
 import random
 
-def partition(arr, low, high):
-    pivot = arr[high]
-    i = low - 1
+test_array = [1, 3, 7, 3, 4, 5, 6, 7, 9, 7, 5, 8, 4, 2, 1, 1, 2, 4]
 
-    for j in range(low, high):
-        if arr[j] <= pivot:
-            i += 1
-            arr[i], arr[j] = arr[j], arr[i]
-
-    arr[i + 1], arr[high] = arr[high], arr[i + 1]
-    return i + 1
-
-def quickselect(arr, low, high, k):
-    if low < high:
-        pivot_index = partition(arr, low, high)
-        if pivot_index == k:
-            return arr[pivot_index]
-        elif pivot_index > k:
-            return quickselect(arr, low, pivot_index - 1, k)
-        else:
-            return quickselect(arr, pivot_index + 1, high, k)
-    return arr[low]
-
-def find_kth_smallest(arr, k):
-    if k < 1 or k > len(arr):
-        return None
-    return quickselect(arr, 0, len(arr) - 1, k - 1)
+# Partitions a list given as argument into two subsets, depending on the value of the pivot.
+def partition(number_list, left, right, pivot_index):
+    pivot = number_list[pivot_index]
+    number_list[pivot_index], number_list[right] = number_list[right], number_list[pivot_index] # The pivot is put at the end of the subset.
+    partition_index = left # The partition index is put at the beginning of the subset.
+    for i in range(left, right - 1):
+        if number_list[i] < pivot:
+            number_list[partition_index], number_list[i] = number_list[i], number_list[partition_index] # If the current value is smaller than the pivot, put it "behind" the partition index
+            partition_index = partition_index + 1
+    number_list[right], number_list[partition_index] = number_list[partition_index], number_list[right] 
+    return partition_index # Put the partition index at the end of the subset and returns it. This ensures that every value left of the partition index are smaller than the pivot.
 
 # Performance test on an unsorted array of 10000 random numbers (between 1 and 10000), and looking for a random kth smallest number in the array.
-def find_kth_smallest_wrapper() :
-    array = [random.randint(1,10000) for _ in range(10000)]
-    k = random.randint(1,10000) 
-    find_kth_smallest(array, k)
+#def find_kth_smallest_wrapper() :
+#    array = [random.randint(1,10000) for _ in range(10000)]
+#    k = random.randint(1,10000) 
+#    find_kth_smallest(array, k)
 
-runtime_performance = timeit.timeit(stmt=find_kth_smallest_wrapper, number=100, globals=globals())
-print(f"This took {runtime_performance} seconds for 100 loops.")
+#runtime_performance = timeit.timeit(stmt=find_kth_smallest_wrapper, number=100, globals=globals())
+#print(f"This took {runtime_performance} seconds.")
+
+print(partition(test_array, 0, len(test_array) -1, 11))
