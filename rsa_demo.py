a11a8d7651f8251a983ed9c8eb9aa39c4563e498
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2023-10-25 11:41:21 +0800

    Made a more polished version of the quickselect algorithm, with a performance test.

--- rsa_demo.py
+++ rsa_demo.py
@@ -2,28 +2,45 @@
 # It compares the performance of those three algorithms using Timeit (more information on : https://docs.python.org/3/library/timeit.html#module-timeit)
 import timeit
 import random
+import pdb
 
-test_array = [1, 3, 7, 3, 4, 5, 6, 7, 9, 7, 5, 8, 4, 2, 1, 1, 2, 4]
+test_array = [random.randint(1, 100) for _ in range(20)]
 
-# Partitions a list given as argument into two subsets, depending on the value of the pivot.
 def partition(number_list, left, right, pivot_index):
     pivot = number_list[pivot_index]
     number_list[pivot_index], number_list[right] = number_list[right], number_list[pivot_index] # The pivot is put at the end of the subset.
-    partition_index = left # The partition index is put at the beginning of the subset.
-    for i in range(left, right - 1):
-        if number_list[i] < pivot:
-            number_list[partition_index], number_list[i] = number_list[i], number_list[partition_index] # If the current value is smaller than the pivot, put it "behind" the partition index
-            partition_index = partition_index + 1
-    number_list[right], number_list[partition_index] = number_list[partition_index], number_list[right] 
-    return partition_index # Put the partition index at the end of the subset and returns it. This ensures that every value left of the partition index are smaller than the pivot.
+    partition_index = left
+    for i in range(left, right):
+        if number_list[i] <= pivot:
+            number_list[i], number_list[partition_index] = number_list[partition_index], number_list[i]
+            partition_index += 1
+    number_list[right], number_list[partition_index] = number_list[partition_index], number_list[right]
+    return partition_index
 
-# Performance test on an unsorted array of 10000 random numbers (between 1 and 10000), and looking for a random kth smallest number in the array.
-#def find_kth_smallest_wrapper() :
-#    array = [random.randint(1,10000) for _ in range(10000)]
-#    k = random.randint(1,10000) 
-#    find_kth_smallest(array, k)
+def quickSelect(number_list, left, right, k, starting_pivot = None):
+    if left == right: # If there's only one element in the subset, return it.
+        return number_list[left]
+    pivot_index = random.randint(left, right) if starting_pivot is None else starting_pivot # Determines the pivot. If no starting pivot is specified, it is choosed at random. 
+    pivot_index = partition(number_list, left, right, pivot_index)
+    if pivot_index == k : # If the pivot index is equal to k, we found the kth smallest.
+        return number_list[k]
+    elif k < pivot_index:   # If the pivot is bigger than k, look for it in the right subset. Else, look for it in the left subset.
+        return quickSelect(number_list, left, pivot_index - 1, k)
+    else:
+        return quickSelect(number_list, pivot_index + 1, right, k) 
 
-#runtime_performance = timeit.timeit(stmt=find_kth_smallest_wrapper, number=100, globals=globals())
-#print(f"This took {runtime_performance} seconds.")
+def findKthSmallestQuickSelect(number_list = test_array, k = random.randint(0, len(test_array) - 1)):
+    return quickSelect(number_list, 0, len(number_list) - 1, k)
 
-print(partition(test_array, 0, len(test_array) -1, 11))
+runtime_performance = timeit.timeit(stmt=findKthSmallestQuickSelect, number=10000, globals=globals())
+print(f"This took {runtime_performance} seconds for 10000 loops.")
+
+random_array = [random.randint(1, 100) for _ in range(20)]
+prompt = random.randint(0, 19)
+print("\nPROMPT = " + str(prompt))
+kth_smallest = findKthSmallestQuickSelect(random_array, prompt)
+for n in random_array:
+    print(n, end=" ")
+print()
+sorted_test = sorted(random_array)
+print(str(sorted_test[prompt]) + " = " +  str(kth_smallest) + " ? " + str(sorted_test[prompt] == kth_smallest)) 
